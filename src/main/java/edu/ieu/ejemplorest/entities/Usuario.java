599c34a0dcc84b961e804b830d9ed45e9b8c280d
IsraelOsorioo <osorioisrael274@gmail.com>
2021-08-04 23:26:20 -0500

    Completado REST JPA

--- src/main/java/edu/ieu/ejemplorest/entities/Usuario.java
+++ src/main/java/edu/ieu/ejemplorest/entities/Usuario.java
@@ -1,11 +1,30 @@
 package edu.ieu.ejemplorest.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 	
-	private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column
 	private String email;
+	
+	@Column
 	private String password;
+	
+	@Column(name = "nombre")
 	private String nombre;
+	
+	@Column
 	private String apellidos;
 	
 	
@@ -14,7 +33,7 @@ public class Usuario {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Usuario(int id, String email, String password, String nombre, String apellidos) {
+	public Usuario(Integer id, String email, String password, String nombre, String apellidos) {
 		super();
 		this.id = id;
 		this.email = email;
@@ -23,10 +42,10 @@ public class Usuario {
 		this.apellidos = apellidos;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 	public String getEmail() {
@@ -53,7 +72,5 @@ public class Usuario {
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	
-	
 
 }
